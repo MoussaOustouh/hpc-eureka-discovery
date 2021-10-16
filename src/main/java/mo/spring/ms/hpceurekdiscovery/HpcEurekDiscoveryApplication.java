a81575ca979b99818ce01aae3acb682c8e6b5a5b
Moussa OUSTOUH <moussa.oustouh@gmail.com>
2021-10-16 18:33:16 +0100

    @EnableEurekaServer and properties to the 'application.properties' file

--- src/main/java/mo/spring/ms/hpceurekdiscovery/HpcEurekDiscoveryApplication.java
+++ src/main/java/mo/spring/ms/hpceurekdiscovery/HpcEurekDiscoveryApplication.java
@@ -2,8 +2,10 @@ package mo.spring.ms.hpceurekdiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class HpcEurekDiscoveryApplication {
 
     public static void main(String[] args) {
